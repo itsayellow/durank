549a778e2e4ef62a3c50a68c4a32cd886336efd4
Matthew Clapp <itsayellow+dev@gmail.com>
2020-11-22 20:34:01 -0800

    Change to pep517 structure.

--- setup.py
+++ setup.py
@@ -1,30 +1,14 @@
-# setup for durank package
+#!/usr/bin/env python3
 
-import os.path
-from setuptools import setup
+import sys
 
-here = os.path.abspath(os.path.dirname(__file__))
+from setuptools import setup  # type: ignore
 
-setup(
-        name='durank',
-        version='0.1',
-        description='disk usage with ranking',
-        author='Matthew Clapp',
-        author_email='itsayellow+dev@gmail.com',
-        license='MIT',
-        classifiers=[
-            'Development Status :: 3 - Alpha',
-            'Intended Audience :: Developers',
-            'License :: OSI Approved :: MIT License',
-            'Programming Language :: Python :: 3'
-            ],
-        keywords='du ranking usage',
-        py_modules=['durank'],
-        entry_points={
-            'console_scripts':[
-                'durank=durank:cli'
-                ]
-            },
-        python_requires='>=3',
-        )
+if sys.version_info < (3, 6, 0):
+    sys.exit(
+        "Python 3.6 or later is required. "
+        "See https://github.com/pipxproject/pipx "
+        "for installation instructions."
+    )
 
+setup()
